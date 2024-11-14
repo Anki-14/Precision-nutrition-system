bfc17f7a9a536d4288c2e29cef3a0085bb6837fc
ANKITA GURSALI <142939251+Anki-14@users.noreply.github.com>
2024-11-14 21:11:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import joblib  # or any other library you're using to load your model
 app = Flask(__name__)
 
 # Load the machine learning model
-model = joblib.load('Precision-nutrition-system-main\Food_nutrition_analysis.py')  # Adjust the path
+model = joblib.load('Precision-nutrition-system-main/Food_nutrition_analysis.py')
+  # Adjust the path
 
 # Define the prediction route
 @app.route('/predict', methods=['POST'])
